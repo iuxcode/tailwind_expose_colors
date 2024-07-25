ce64b36d86e2ea51018c8faa378ab87e7e569c55
iuxcode <charlottabade@gmail.com>
2024-07-25 09:36:44 +0000

    init

---
+++ custom_colors.js
@@ -0,0 +1,35 @@
+function exportCustomColors({ addBase, theme }) {
+  function extractColorVars(colorObj, colorGroup = "") {
+    return Object.keys(colorObj).reduce((vars, colorKey) => {
+      const value = colorObj[colorKey];
+      const cssVariable =
+        colorKey === "DEFAULT"
+          ? `--color${colorGroup}`
+          : `--color${colorGroup}-${colorKey}`;
+
+      const newVars =
+        typeof value === "string"
+          ? { [cssVariable]: value }
+          : extractColorVars(value, `-${colorKey}`);
+
+      return { ...vars, ...newVars };
+    }, {});
+  }
+
+  function filterCustomColors(colors) {
+    const customColors = {};
+    const customColorKeys = ["custom1", "custom2"]; // Add your custom color keys here
+
+    customColorKeys.forEach((key) => {
+      if (colors[key]) {
+        customColors[key] = colors[key];
+      }
+    });
+
+    return customColors;
+  }
+
+  addBase({
+    ":root": extractColorVars(filterCustomColors(theme("colors"))),
+  });
+}
